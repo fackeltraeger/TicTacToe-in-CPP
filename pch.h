71def337439aafae12fca4817294c1a2828e90a1
pavan <pavan.dogiparti@secude.com>
2026-01-03 02:01:26 +0530

    moved the code from cpp-file to header-file for readabilty.

--- pch.h
+++ pch.h
@@ -14,3 +14,48 @@ int g_boardLopper = 0;
 int g_IntPlyerWon = 0;
 bool g_bPlayOrExit = false;
 
+
+
+void displayOnScreen()
+{
+    cout << "-----------" << endl;
+    for (auto& i : vec)
+    {
+        cout << " " << i[0] << " | " << i[1] << " | " << i[2] << endl;
+        g_boardLopper++;
+        if (g_boardLopper < 3)
+            cout << " - | - | - " << endl;
+        else
+            g_boardLopper = 0;
+    }
+    cout << "-----------" << endl;
+}
+
+bool isNotOccupied(int& IntVal)
+{
+    int row = (IntVal - 1) / 3;
+    int col = (IntVal - 1) % 3;
+
+    if ((vec[row][col] == 'O' || vec[row][col] == 'X'))
+        return false;
+    else
+    {
+        vec[row][col] = g_ChCurrentMarker; // if not occupied place the value to it.
+        return true;
+    }
+}
+
+int bCheckWinner()
+{
+    for (size_t i = 0; i < 3; i++)
+    {
+        if ((vec[0][i] == vec[1][i] && vec[1][i] == vec[2][i]) || (vec[i][0] == vec[i][1] && vec[i][1] == vec[i][2]))
+            return g_IntCurrentPlayer;
+    }
+    if (vec[0][0] == vec[1][1] && vec[1][1] == vec[2][2])
+        return g_IntCurrentPlayer;
+    else if (vec[2][0] == vec[1][1] && vec[1][1] == vec[0][2])
+        return g_IntCurrentPlayer;
+    else
+        return -1;
+}
